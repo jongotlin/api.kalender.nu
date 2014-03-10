6b44f922d2e15da75efb7912ea2d81f6a4f3b991
Jon Gotlin <jon@jon.se>
2014-03-10 22:41:59 +0100

    first commit

---
+++ src/JGI/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace JGI\AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Nelmio\ApiDocBundle\Annotation as ApiDoc;
+
+class DefaultController extends Controller
+{
+    /**
+     * Get date
+     *
+     * <h4>Response</h4>
+     * <pre>
+     * {
+     *   "date":"2014-03-09",
+     *   "red_day":true
+     * }
+     * </pre>
+     *
+     * @ApiDoc\ApiDoc(
+     *  section="Date",
+     *  statusCodes={
+     *    200="Returned when successful",
+     *    400="Returned when date is invalid"
+     *  }
+     * )
+     * @Rest\Get("/dates/{date}")
+     * @Rest\View()
+     */
+    public function dateAction($date)
+    {
+        $this->forward400IfDateIsNotValid($date);
+        $date = $this->get('jgi.swedish_dates.datemanager')->getDate(new \Datetime($date));
+
+        return [
+            'date' => $date->getDateTime()->format('Y-m-d'),
+            'red_day' => $date->isRedDay(),
+        ];
+    }
+
+    /**
+     * @param string $date
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
+     */
+    protected function forward400IfDateIsNotValid($date)
+    {
+        try {
+            new \DateTime($date);
+        } catch (\Exception $e) {
+            throw new BadRequestHttpException(sprintf('Date "%s" is not valid', $date));
+        }
+    }
+}
---
+++ src/JGI/AppBundle/JGIAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JGI\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JGIAppBundle extends Bundle
+{
+}
